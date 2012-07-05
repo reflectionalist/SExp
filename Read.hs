67157b357fe841de68d69b07bc5b5b09f1f3a4d2
Yi Dai <plmday@gmail.com>
2012-07-05 21:40:46 +0200

    Implemented the read function for S-expressions.

--- Read.hs
+++ Read.hs
@@ -1,4 +1,5 @@
 module Read
+  ( readSExp )
 where
 
 
@@ -6,6 +7,7 @@ import Data.Char
 import System.IO
 
 
+-- get a character without echoing it
 hGetCh :: Handle -> IO Char
 hGetCh hdl = do
   hSetEcho hdl False
@@ -14,44 +16,29 @@ hGetCh hdl = do
   return c
 
 readSExp :: Handle -> IO String
-readSExp hdl = readSE hdl True 0 ""
-  where readSE hdl out lvl str = do
-          if not (str == "") && (out && lvl == 0)
-             then return (reverse str)
-             else do c <- hGetCh hdl
-                     if isSpace c
-                        then do putChar c
-                                readSE hdl (if out then out else True) lvl (c : str)
-                        else case c of
-                               '\DEL' -> do putStr "\b\b"
-                                            readSE
-                               '(' -> readSE hdl True (lvl - 1) (c : str)
-                               ')' -> readSE hdl True (lvl + 1) (c : str)
-                               _   -> readSE hdl False lvl (c : str)
-
-readAtom :: Handle -> IO String
-readAtom hdl = readAt hdl ""
-  where readAt hdl str = do
-          c <- hLookAhead hdl
-          if isSpace c || c `elem` "()"
-             then return (reverse str)
-             else readAt hdl (c : str)
-
-
-getCh :: IO Char
-getCh =  do hSetEcho stdin False
-            c <- getChar
-            hSetEcho stdin True
-            return c
-
-readLine :: IO String
-readLine =  f ""
-            where
-              f xs =  do c <- getCh
-                         handle xs c
-              handle [] '\DEL' = f ""
-              handle xs '\DEL' = do putChar '\b'
-                                    f (init xs)
-              handle xs c      = do putChar c
-                                    f (xs ++ [c])
+readSExp hdl = readSE hdl 0 0 ""
+  where readSE hdl cnt lvl str = do
+          c <- hGetCh hdl
+          case c of
+            '\DEL' | str == "" -> readSE hdl cnt lvl str
+                   | otherwise -> do putChar '\b'
+                                     case head str of
+                                       '('           -> readSE hdl (cnt - 1) (lvl + 1) (tail str)
+                                       ')'           -> readSE hdl (cnt - 1) (lvl - 1) (tail str)
+                                       c | isSpace c -> readSE hdl cnt lvl (tail str)
+                                         | otherwise -> readSE hdl (cnt - 1) lvl (tail str)
+            '\n' | cnt /= 0 && lvl == 0
+                               -> do putChar c
+                                     return (reverse str)
+            '('                -> do putChar c
+                                     readSE hdl (cnt + 1) (lvl - 1) (c : str)
+            ')'                -> do putChar c
+                                     readSE hdl (cnt + 1) (lvl + 1) (c : str)
+            _ | isSpace c      -> do putChar c
+                                     readSE hdl cnt lvl 
+                                          $ if str == "" || isSpace (head str)
+                                               then str
+                                               else c : str
+              | otherwise      -> do putChar c
+                                     readSE hdl (cnt + 1) lvl (c : str)
 
