6a017a187539ac1e2a9c035e265af034b7b151ba
Yi Dai <plmday@gmail.com>
2012-07-06 10:39:46 +0200

    Immigrated the definition of the read function for S-expressions; refactored a bit.

--- SExp.hs
+++ SExp.hs
@@ -10,6 +10,7 @@ import Text.Parsec
 import Text.Parsec.String
 import Control.Monad
 import Data.Char
+import System.IO
 
 
 type Name = String
@@ -18,22 +19,13 @@ data SExp
   = Atom Name
   | List [SExp]
 
+
 parseSExp :: Parser SExp
 parseSExp = parseAtom
        <|> do char '(' >> spaces
               e <- parseList
               spaces >> char ')'
               return e
-
-serializeSExp :: SExp -> String
-serializeSExp = show
-
-instance Show SExp where
-  show (Atom s)  = s
-  show (List []) = "()"
-  show (List es) = "(" ++ showList es ++ ")"
-    where showList [e]      = show e
-          showList (e : es) = show e ++ " " ++ showList es
 
 parseAtom :: Parser SExp
 parseAtom = liftM Atom . many1 $ atomChar
@@ -48,4 +40,53 @@ atomChar = satisfy $ \c -> not (isSpace c || elem c "()")
 
 spaces1 :: Parser ()
 spaces1 = skipMany1 space
+
+serializeSExp :: SExp -> String
+serializeSExp = show
+
+instance Show SExp where
+  show (Atom s)  = s
+  show (List []) = "()"
+  show (List es) = "(" ++ showList es ++ ")"
+    where showList [e]      = show e
+          showList (e : es) = show e ++ " " ++ showList es
+
+
+hReadSExp :: Handle -> Handle -> IO String
+hReadSExp ihd ohd = hReadSE ihd ohd 0 0 ""
+  where hReadSE ihd ohd cnt lvl str = do
+          c <- hGetCh ihd
+          case c of
+            '\DEL' | str == "" -> hReadSE ihd ohd cnt lvl str
+                   | otherwise -> do hPutStr ohd "\b \b"
+                                     case head str of
+                                       '('           -> hReadSE ihd ohd (cnt - 1) (lvl + 1) (tail str)
+                                       ')'           -> hReadSE ihd ohd (cnt - 1) (lvl - 1) (tail str)
+                                       c | isSpace c -> hReadSE ihd ohd cnt lvl (tail str)
+                                         | otherwise -> hReadSE ihd ohd (cnt - 1) lvl (tail str)
+            '\n' | cnt /= 0 && lvl == 0
+                               -> do hPutChar ohd c
+                                     return (reverse str)
+            '('                -> do hPutChar ohd c
+                                     hReadSE ihd ohd (cnt + 1) (lvl - 1) (c : str)
+            ')'                -> do hPutChar ohd c
+                                     hReadSE ihd ohd (cnt + 1) (lvl + 1) (c : str)
+            _ | isSpace c      -> do hPutChar ohd c
+                                     hReadSE ihd ohd cnt lvl
+                                          $ if str == "" || isSpace (head str)
+                                               then str
+                                               else c : str
+              | otherwise      -> do hPutChar ohd c
+                                     hReadSE ihd ohd (cnt + 1) lvl (c : str)
+
+readSExp :: IO String
+readSExp = hReadSExp stdin stdout
+
+-- get a character without echoing it
+hGetCh :: Handle -> IO Char
+hGetCh hdl = do
+  hSetEcho hdl False
+  c <- hGetChar hdl
+  hSetEcho hdl True
+  return c
 
