5ed1141c582f1728c4f7de06a5ff832b3905d451
Yi Dai <plmday@gmail.com>
2012-07-10 11:36:44 +0200

    Prepended an EOF test before attempting to get a character.

--- SExp.hs
+++ SExp.hs
@@ -56,24 +56,27 @@ serializeSExp (List sexps) = "(" ++ serializeSExps sexps ++ ")"
 hReadSExp :: Handle -> Handle -> IO String
 hReadSExp ihd ohd = hReadSE ihd ohd 0 0 ""
   where hReadSE ihd ohd cnt lvl str = do
-          c <- hGetChar ihd
-          case c of
-            '\DEL' | str == "" -> hReadSE ihd ohd cnt lvl str
-                   | otherwise -> do hPutStr ohd "\b \b"
-                                     case head str of
-                                       '('           -> hReadSE ihd ohd (cnt - 1) (lvl + 1) (tail str)
-                                       ')'           -> hReadSE ihd ohd (cnt - 1) (lvl - 1) (tail str)
-                                       c | isSpace c -> hReadSE ihd ohd cnt lvl (tail str)
-                                         | otherwise -> hReadSE ihd ohd (cnt - 1) lvl (tail str)
-            '\n' | cnt /= 0 && lvl == 0
-                               -> return (reverse str)
-            '('                -> hReadSE ihd ohd (cnt + 1) (lvl - 1) (c : str)
-            ')'                -> hReadSE ihd ohd (cnt + 1) (lvl + 1) (c : str)
-            _ | isSpace c      -> hReadSE ihd ohd cnt lvl
-                                        $ if str == "" || isSpace (head str)
-                                             then str
-                                             else c : str
-              | otherwise      -> hReadSE ihd ohd (cnt + 1) lvl (c : str)
+          eof <- isEOF
+          if eof
+             then return ""
+             else do c <- hGetChar ihd
+                     case c of
+                       '\DEL' | str == "" -> hReadSE ihd ohd cnt lvl str
+                              | otherwise -> do hPutStr ohd "\b \b"
+                                                case head str of
+                                                  '('           -> hReadSE ihd ohd (cnt - 1) (lvl + 1) (tail str)
+                                                  ')'           -> hReadSE ihd ohd (cnt - 1) (lvl - 1) (tail str)
+                                                  c | isSpace c -> hReadSE ihd ohd cnt lvl (tail str)
+                                                    | otherwise -> hReadSE ihd ohd (cnt - 1) lvl (tail str)
+                       '\n' | cnt /= 0 && lvl == 0
+                                          -> return (reverse str)
+                       '('                -> hReadSE ihd ohd (cnt + 1) (lvl - 1) (c : str)
+                       ')'                -> hReadSE ihd ohd (cnt + 1) (lvl + 1) (c : str)
+                       _ | isSpace c      -> hReadSE ihd ohd cnt lvl
+                                                   $ if str == "" || isSpace (head str)
+                                                        then str
+                                                        else c : str
+                         | otherwise      -> hReadSE ihd ohd (cnt + 1) lvl (c : str)
 
 readSExp :: IO String
 readSExp = hReadSExp stdin stdout
